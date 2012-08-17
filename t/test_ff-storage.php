e19204c2edca59188b1facf2148ce4d75a7de6e7
Alessandro Dotti Contra <alessandro@hyboria.org>
2012-08-17 20:51:56 +0200

    Created test unit placeholder.

---
+++ t/test_ff-storage.php
@@ -0,0 +1,27 @@
+<?php
+require_once("../lib/ff-storage.php");
+
+print "***** FF-STORAGE TEST UNIT\n";
+print "===> Basic tests\n";
+print "[TODO] Create a storage (strings)\n";
+print "[TODO] Create a storage (objects)\n";
+print "[TODO] Add a string\n";
+print "[TODO] Add an object\n";
+print "[TODO] Update strings\n";
+print "[TODO] Update objects\n";
+print "[TODO] Remove strings\n";
+print "[TODO] Remove objects\n";
+print "[TODO] Find strings\n";
+print "[TODO] Find objects\n";
+print "[TODO] Count elements in storage (strings)\n";
+print "[TODO] Count elements in storage (objects)\n";
+print "===> Failure tests\n";
+print "[TODO] Create a storage (wrong type)\n";
+print "[TODO] Add item (string -> objects)\n";
+print "[TODO] Add item (object -> strings)\n";
+print "[TODO] Add object (malformed object)\n";
+print "[TODO] Update objects (malformed old)\n";
+print "[TODO] Update objects (malformed new)\n";
+print "[TODO] Remove objects (malformed object)\n";
+print "[TODO] Find objects (malformed object)\n";
+?>
